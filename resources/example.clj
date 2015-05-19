e9cb517b3de7e9e94dd15600e33ba8692ec22385
Aaron Lebo <aaron.matthew.lebo@gmail.com>
2015-05-19 14:55:20 -0500

    example.pr

--- resources/example.clj
+++ resources/example.clj
@@ -1,4 +1,10 @@
 (defn hello [name & :keys [city] :or {city "Dallas"}]  
-  (let [message (join " " ["Hello," name "from" city])] 
+  (println (+ (count name) 1))  ;; because we can  
+  (let [message (string/join " " ["Hello," name "from" city])] 
     (println message)  
     message))
+
+
+(hello "Howard Hughes" "Houston")
+(hello "Eleanor Roosevelt" "New York City")
+(hello "The Linux Users Group @ UT Dallas")
